dcfc62c5a02841f8060def0f6771ef8caca03c09
okplanb <26038255+ok-plan-b@users.noreply.github.com>
2023-12-15 03:03:15 +0400

    feat: [S1] edit modal implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useRef } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 import { Paper, Tooltip, FormControlLabel, Checkbox, Dialog, DialogActions,
     DialogContent, DialogContentText, DialogTitle, Typography, LinearProgress,
-    Button, useMediaQuery, Box } from "@mui/material";
+    Button, IconButton, useMediaQuery, Box, Input } from "@mui/material";
 
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { useTheme } from '@mui/material/styles';
 
-import { basicList, storage_key, tooptip_offset, total_percent } from ":src/constants";
+import { debounce } from ":src/helpers";
+import { basicList, debounce_delay, storage_key,
+    task_input_limit, tooptip_offset, total_percent } from ":src/constants";
 
 import "./App.scss";
 
 type Task = {
     description: string;
     checked: boolean;
+    key: string;
 };
 
-const initialTasks: Task[] = basicList.map(description => {
-    return { description, checked: false }
-});
-initialTasks[0].checked = true;
+function getInitialTasks() {
+    const initialTasks: Task[] = basicList.map(description => {
+        return { description, checked: false, key: uuidv4() }
+    });
+    initialTasks[0].checked = true;
+    return initialTasks;
+}
 
 const saved_tasks = localStorage.getItem(storage_key);
 
@@ -33,11 +42,14 @@ const calcProgress = (tasks: Task[]) => {
 }
 
 export default function App(): JSX.Element {
-    const [tasks, setTasks] = useState<Task[]>(saved_tasks ? JSON.parse(saved_tasks) : initialTasks);
+    const [tasks, setTasks] = useState<Task[]>(saved_tasks ? JSON.parse(saved_tasks) : getInitialTasks());
+    const [editedTasks, setEditedTasks] = useState<Task[]>([]);
     const [progress, setProgress] = useState(calcProgress(tasks));
-    const [open, setOpen] = useState(false);
+    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+    const [isConfirmOpen, setIsConfirmOpen] = useState(false);
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+    const addButtonRef = useRef<HTMLButtonElement>(null);
   
     const updateTasks = (newTasks: Task[]) => {
         localStorage.setItem(storage_key, JSON.stringify(newTasks));
@@ -52,22 +64,72 @@ export default function App(): JSX.Element {
     }, [tasks]);
 
     const handleRestart = useCallback(() => {
-        const newTasks = initialTasks;
+        const newTasks = tasks.slice();
+        newTasks.forEach(task => task.checked = false);
         updateTasks(newTasks);
-        setOpen(false);
-    }, []);
+        setIsConfirmOpen(false);
+    }, [tasks]);
 
-    const handleDialogOpen = () => {
-        setOpen(true);
+    const handleConfirmOpen = () => {
+        setIsConfirmOpen(true);
     };
-    
-    const handleDialogClose = () => {
-        setOpen(false);
+
+    const handleConfirmClose = () => {
+        setIsConfirmOpen(false);
+    };
+
+    const handleEdit = () => {
+        setEditedTasks(tasks.slice());
+        setIsEditDialogOpen(true);
+    };
+
+    const handleCancelEdit = () => {
+        setIsEditDialogOpen(false);
+    };
+
+    const handleAddNewTask = () => {
+        const newTasks = editedTasks.slice();
+        newTasks.push({
+            key: uuidv4(),
+            description: '',
+            checked: false
+        });
+        setEditedTasks(newTasks);
+        addButtonRef.current?.scrollIntoView({behavior: 'smooth', inline: 'end' });
+    };
+
+    const handleSaveChanges = () => {
+        setIsEditDialogOpen(false);
+        const newTasks = editedTasks.filter(task => task.description);
+        updateTasks(newTasks);
+    };
+
+    const debouncedUpdate = debounce((newDescription: string, key: string) => {
+        const tasks = editedTasks.map(task => {
+            if (task.key === key) {
+                task.description = newDescription;
+            }
+            return task;
+        });
+        setEditedTasks(tasks);
+    }, debounce_delay);
+
+    const handleTaskEdit = (value: string, key: string) => {
+        debouncedUpdate(value, key);
+    };
+
+    const handleDeleteTask = (key: string) => {
+        const tasks = editedTasks.filter(task => task.key !== key);
+        setEditedTasks(tasks);
     };
 
     return (
         <>
-            <Typography variant="h2" component="h1" align="center">
+            <Typography
+                variant="h2"
+                component="h1"
+                align="center"
+            >
                 <Tooltip
                     title="is Georgian for 'List'"
                     placement="top"
@@ -88,29 +150,116 @@ export default function App(): JSX.Element {
                     </Box>
                 </Tooltip>
             </Typography>
-            <Paper variant="outlined" component="main">
-                {tasks.map((task, index) => (
-                    <FormControlLabel
-                        className="item"
-                        key={index}
-                        label={task.description}
-                        control={
-                            <Checkbox
-                                checked={task.checked}
-                                onChange={() => toggleTask(index)}
-                            />
-                        }
-                    />
-                ))}
-            </Paper>
+            
+            { tasks.length === 0 ? (
+                <Typography
+                    variant="h4"
+                    component="h4"
+                    align="center"
+                    marginTop="2rem"
+                >
+                    It is so empty here... What&apos;s the plan? :)
+                </Typography>
+            ) : (
+                <Paper variant="outlined" component="main">
+                    {tasks.map((task, index) => (
+                        <FormControlLabel
+                            className="item"
+                            key={task.key}
+                            label={task.description}
+                            control={
+                                <Checkbox
+                                    checked={task.checked}
+                                    onChange={() => toggleTask(index)}
+                                />
+                            }
+                        />
+                    ))}
+                </Paper>
+            )}
+
             <Box display="flex" justifyContent="center" marginTop="2rem">
-                <Button variant="outlined" onClick={handleDialogOpen}>Restart</Button>
+                <Box display="flex" marginRight="2rem">
+                    <Button variant="outlined" onClick={handleEdit}>
+                        Edit
+                    </Button>
+                </Box>
+                <Button variant="outlined" onClick={handleConfirmOpen}>
+                    Restart
+                </Button>
             </Box>
+
             <LinearProgress variant="determinate" value={progress} className="progress" />
+
+            {/* Editing */}
+
             <Dialog
                 fullScreen={fullScreen}
-                open={open}
-                onClose={handleDialogClose}
+                open={isEditDialogOpen}
+                onClose={handleCancelEdit}
+                aria-labelledby="responsive-dialog-title"
+            >
+                <DialogTitle id="responsive-dialog-title">
+                    Edit tasks
+                </DialogTitle>
+                <DialogContent sx={ fullScreen ? undefined : { minWidth: '500px'}}>
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        marginBottom="1rem"
+                    >
+                        {editedTasks.map(item =>
+                            <Box key={item.key} display="flex">
+                                <Input
+                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                        handleTaskEdit(event.target.value, item.key)
+                                    }}
+                                    defaultValue={item.description}
+                                    fullWidth
+                                    sx={{ marginBottom: "0.5rem" }}
+                                    inputProps={{
+                                        'aria-label': 'Task description',
+                                        maxLength: task_input_limit
+                                    }}
+                                />
+                                <IconButton
+                                    aria-label="delete"
+                                    size="medium"
+                                    onClick={() => {
+                                        handleDeleteTask(item.key)
+                                    }}
+                                >
+                                    <DeleteIcon fontSize="medium" />
+                                </IconButton>
+                            </Box>
+                        )}
+                        <Button
+                            variant="outlined"
+                            startIcon={<AddIcon />}
+                            sx={{ marginTop: "0.5rem", marginBottom: "0.5rem" }}
+                            ref={addButtonRef}
+                            onClick={handleAddNewTask}
+                        >
+                            New task
+                        </Button>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={handleCancelEdit}>
+                        Cancel
+                    </Button>
+                    <Button onClick={handleSaveChanges} variant="contained" autoFocus>
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            {/* Restart Confirmation */}
+
+            <Dialog
+                fullScreen={fullScreen}
+                open={isConfirmOpen}
+                onClose={handleConfirmClose}
                 aria-labelledby="responsive-dialog-title"
             >
                 <DialogTitle id="responsive-dialog-title">
@@ -122,7 +271,7 @@ export default function App(): JSX.Element {
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button autoFocus onClick={handleDialogClose}>
+                    <Button autoFocus onClick={handleConfirmClose}>
                         False alarm
                     </Button>
                     <Button onClick={handleRestart} variant="contained" autoFocus>
